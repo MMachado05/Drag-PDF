d7763b84f714ff66f36b8fa27d55593225374e9e
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2025-04-07 15:15:02 +0200

    Reorganized Firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,11 @@
 import 'package:drag_pdf/core/theme.dart';
-import 'package:drag_pdf/core/utils.dart';
 import 'package:drag_pdf/views/pdf_combiner_screen.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'firebase_options.dart';
 
 Future<void> main() async {
-  if (kReleaseMode) {
-    await dotenv.load(fileName: '.env');
-    if (Utils.checkEnvFile()) {
-      await Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
-      );
-    }
-  }
-    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
-
-    PlatformDispatcher.instance.onError = (error, stack) {
-      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-      return true;
-    };
-
+  await DefaultFirebaseOptions.initializeFirebase();
   runApp(const MyApp());
 }
 
